a443188a0b6237f94c13bba310785d57532acbb3
Uchencho <aloziekelechi17@gmail.com>
2020-05-23 10:55:50 +0100

    created the product model

--- product/models.py
+++ product/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title           = models.CharField(max_length=50)
+    description     = models.TextField()
